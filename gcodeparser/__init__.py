46ad95f2d276f0dff775f5466fa236bda73e3c62
AndyEveritt <andreweveritt@e3d-online.com>
2021-03-02 18:33:37 +0000

    v0.0.5

--- gcodeparser/__init__.py
+++ gcodeparser/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.3"
+__version__ = "0.0.5"
 
 from .gcode_parser import GcodeParser
 from .commands import Commands
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="gcodeparser",
-    version="0.0.3",
+    version="0.0.5",
     include_package_data=True,
     packages=find_packages(),
 
