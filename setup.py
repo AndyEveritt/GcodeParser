7e3eaeb4ce48a9e2bc9a8322fd955915fff3449d
AndyEveritt <andreweveritt@e3d-online.com>
2021-03-03 14:45:05 +0000

    v0.0.6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="gcodeparser",
-    version="0.0.5",
+    version="0.0.6",
     include_package_data=True,
     packages=find_packages(),
 
