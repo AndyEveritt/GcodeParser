95aa2b9ceb2c406336718eb76dc58f7f7fc5d5a0
AndyEveritt <38423143+AndyEveritt@users.noreply.github.com>
2023-07-31 10:56:20 +0100

    somehow the versions got all wack

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="gcodeparser",
-    version="0.0.10",
+    version="0.1.1",
     include_package_data=True,
     packages=find_packages(),
 
